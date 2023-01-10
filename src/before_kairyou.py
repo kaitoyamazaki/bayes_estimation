0bb9b79510ac3727d6ab27bb3c1eacc1213a9e3e
kaitoyamazaki <reflect018@yahoo.co.jp>
2023-01-10 14:40:45 +0900

    update code

--- src/before_kairyou.py
+++ src/before_kairyou.py
@@ -76,30 +76,35 @@ class Bayes():
         plt.plot(x,first)
         plt.suptitle("Old algorithm probability for 1st time of enforcement")
         plt.xlabel("t (finish rate)")
+        plt.ylabel("probability")
 
         #施行２回目のグラフ
         plt.figure()
         plt.plot(x,second)
         plt.suptitle("Old algorithm probability for 2st time of enforcement")
         plt.xlabel("t (finish rate)")
+        plt.ylabel("probability")
 
         #施行３回目のグラフ
         plt.figure()
         plt.plot(x,third)
         plt.suptitle("Old algorithm probability for 3st time of enforcement")
         plt.xlabel("t (finish rate)")
+        plt.ylabel("probability")
 
         #施行４回目のグラフ
         plt.figure()
         plt.plot(x,forth)
         plt.suptitle("Old algorithm probability for 4st time of enforcement")
         plt.xlabel("t (finish rate)")
+        plt.ylabel("probability")
 
         #施行５回目のグラフ
         plt.figure()
         plt.plot(x,fifth)
         plt.suptitle("Old algorithm probability for 5st time of enforcement")
         plt.xlabel("t (finish rate)")
+        plt.ylabel("probability")
 
         plt.show()
 
