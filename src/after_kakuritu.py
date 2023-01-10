9d3a081eb42ec36c134d97a65676e4aa61a0ce5c
kaitoyamazaki <reflect018@yahoo.co.jp>
2023-01-10 15:19:25 +0900

    update code

--- src/after_kakuritu.py
+++ src/after_kakuritu.py
@@ -64,7 +64,7 @@ class Bayes():
         return data
 
     def answer_probability(self, data, value):
-        print(value, "回目の施行")
+        print("\n",value, "回目の施行")
         print("max probability : ", max(data))
         print("finish rate : ", data.index(max(data)) / 100)
 
@@ -76,22 +76,58 @@ class Bayes():
         #施行１回目のグラフ
         plt.figure()
         plt.plot(x,first)
+        plt.suptitle("new algorithm probability for 1st execution")
+        plt.xlabel("t")
+        plt.ylabel("probability")
+        plt.xticks([0.00, 0.5, 1])
+        plt.yticks([0.00, 0.02, 0.04, 0.06, 0.08])
+        plt.savefig("../figure/新アルゴリズム_施行1回目の確率分布.png")
 
         #施行２回目のグラフ
         plt.figure()
         plt.plot(x,second)
+        plt.xlabel("t")
+        plt.ylabel("probability")
+        plt.xticks([0.00, 0.5, 1])
+        plt.yticks([0.00, 0.02, 0.04, 0.06, 0.08])
+        plt.savefig("../figure/新アルゴリズム_施行1回目の確率分布.png")
+        plt.suptitle("new algorithm probability for 2st execution")
+        plt.xlabel("t")
+        plt.ylabel("probability")
+        plt.xticks([0.00, 0.5, 1])
+        plt.yticks([0.00, 0.02, 0.04, 0.06, 0.08])
+        plt.savefig("../figure/新アルゴリズム_施行2回目の確率分布.png")
 
         #施行３回目のグラフ
         plt.figure()
         plt.plot(x,third)
+        plt.suptitle("new algorithm probability for 3st execution")
+        plt.xlabel("t")
+        plt.ylabel("probability")
+        plt.xticks([0.00, 0.5, 1])
+        plt.yticks([0.00, 0.02, 0.04, 0.06, 0.08])
+        plt.savefig("../figure/新アルゴリズム_施行3回目の確率分布.png")
 
         #施行４回目のグラフ
         plt.figure()
         plt.plot(x,forth)
+        plt.suptitle("new algorithm probability for 4st execution")
+        plt.xlabel("t")
+        plt.ylabel("probability")
+        plt.xticks([0.00, 0.5, 1])
+        plt.yticks([0.00, 0.02, 0.04, 0.06, 0.08])
+        plt.savefig("../figure/新アルゴリズム_施行4回目の確率分布.png")
 
         #施行５回目のグラフ
         plt.figure()
         plt.plot(x,fifth)
+        plt.suptitle("new algorithm probability for 5st execution")
+        plt.xlabel("t")
+        plt.ylabel("probability")
+        plt.xticks([0.00, 0.5, 1])
+        plt.yticks([0.00, 0.02, 0.04, 0.06, 0.08])
+        plt.savefig("../figure/新アルゴリズム_施行5回目の確率分布.png")
+
         plt.show()
 
 
